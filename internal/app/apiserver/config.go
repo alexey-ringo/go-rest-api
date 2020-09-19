f14cafff5638142cdaab7e502a159d95657d63c6
alexey-ringo <alexey.ringo@gmail.com>
2020-09-20 00:42:36 +0300

    logger

--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -3,11 +3,13 @@ package apiserver
 type Config struct {
 	//Адрес, на котором запускается веб-сервер
 	BindAddr string `toml:"bind_addr"`
+	LogLevel string `toml:"log_level"`
 }
 
 //Инициализированный конфиг с дефолтными параметрами
 func NewConfig() *Config {
 	return &Config{
 		BindAddr: ":8081",
+		LogLevel: "debug",
 	}
 }
